6c91dca9cb4af6f727f51ea23f9013beedd0d304
rilzisheree <160282181+rilzisheree@users.noreply.github.com>
2026-05-07 14:34:50 +0300

    Delete bot-js directory

--- bot-js/src/models/LogChannel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const LogChannelSchema = new mongoose.Schema({
-  channelId: { type: String, required: true },
-  setBy:     { type: String, required: true },
-  setAt:     { type: Date, default: Date.now },
-});
-
-export default mongoose.model("LogChannel", LogChannelSchema);
--- bot-js/src/utils/colors.js
+++
@@ -1,4 +0,0 @@
-export const ORANGE = 0xfe6001;
-export const RED = 0xe74c3c;
-export const GREEN = 0x2ecc71;
-export const GRAY = 0x95a5a6;
